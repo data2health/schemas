43e136349cb4bf08eaf66cccf31e006c3d756a98
sookieliao <kevinxin@scripps.edu>
2019-01-18 11:40:27 -0800

    add test to examine biothings schema classes

--- tests.py
+++ tests.py
@@ -13,6 +13,29 @@ class TestBioThingsSchema(unittest.TestCase):
         self.assertEqual(set(self.biothings_schema.keys()),
                          set(["@context", "@graph", "@id"]))
 
+    def test_general_values(self):
+        """Test some values within schema that should be consistent
+        """
+        for _doc in self.biothings_schema['@graph']:
+            self.assertEqual(_doc["http://schema.org/isPartOf"],
+                             {"@id": "http://schema.biothings.io"})
+            self.assertIn(_doc["@type"], ["rdfs:Class", "rdf:Property"])
+            self.assertEqual(_doc["@id"][:27], "http://schema.biothings.io/")
+            self.assertEqual(_doc["@id"][27:], _doc["rdfs:label"])
+            self.assertEqual(len(_doc["rdfs:label"].split(' ')), 1)
+
+    def test_class_specific_features(self):
+        """Test features belong to schema classes
+        """
+        all_classes = [_doc["rdfs:label"] for _doc in self.biothings_schema["@graph"] if _doc["@type"] == "rdfs:Class"] + ["Thing"]
+        for _doc in self.biothings_schema["@graph"]:
+            if _doc["@type"] == "rdfs:Class":
+                self.assertTrue(_doc["rdfs:label"][0].isupper())
+                self.assertTrue(_doc["rdfs:subClassOf"]["@id"][0].isupper())
+                self.assertIn(_doc["rdfs:subClassOf"]["@id"], all_classes)
+
+    
+
 
 if __name__ == '__main__':
     unittest.main()
